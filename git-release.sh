948589e78cda593665fe6217ae5d1c7259a298bc
Fan Jin <fanjin1989@gmail.com>
2019-08-22 16:13:51 -0700

    fix: change script code

--- git-release.sh
+++ git-release.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
+# Detect the script location, so it doesn't need to be run from a specific folder to work.
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd "${DIR}/.."
+
+# Tell the Script to Exit if a Command Returns a Non-Zero Exit Code
+set -e
 
 result=${PWD##*/}
-
 # rm -vr !("dist"|"scripts")
 # shopt -s extglob
-# rm -rv !("dist"|"git-release.sh|node_modules")
+rm -rv !("dist"|"git-release.sh")
 if [ $result = "core" ]
 then
-  cp -r ./dist/core ../
+  cp -r dist/core ../
 else
-  cp -r ./dist/keepalive ../
+  cp -r dist/keepalive ../
 fi
 rm -r dist
