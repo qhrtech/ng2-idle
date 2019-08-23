51e4889810d53520ccdf4936aa5f9dc429a64bbd
Fan Jin <fanjin1989@gmail.com>
2019-08-22 18:49:05 -0700

    fix: update package.json

--- git-release.sh
+++ git-release.sh
@@ -1,29 +1,40 @@
 #!/bin/sh
 # Detect the script location, so it doesn't need to be run from a specific folder to work.
 
-# SOURCE="${BASH_SOURCE[0]}"
-# while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-#   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-#   SOURCE="$(readlink "$SOURCE")"
-#   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-# done
-# DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-# cd "${DIR}"
+cd "${DIR}"
 
 # result=${DIR##*/}
 
 # shopt -s extglob
 # rm -rv !("dist"|"git-release.sh")
+# packagejson=`cat ./package.json`
+# echo $packagejson
+# if [[ $packagejson == *"@ng-idle/core"* ]]; then
+if [VARNAME != "my value"]
+then
+  `cp -r ./dist/core/ ./`
+if
 
-# if [[ ${DIR} == *"core"* ]]; then
-#   `cp -r ./dist/core/ ./`
+if [VARNAME = "my value"]
+then
+  `cp -r ./dist/keepalive/ ./`
+if
+
+export VARNAME="my value"
+
+
 # fi
 
-# if [[ ${DIR} == *"keepalive"* ]]; then
+# if [[ $packagejson == *"@ng-idle/keepalive"* ]]; then
 #   `cp -r ./dist/keepalive/ ./`
 # fi
-
-echo ${DIR} > temp
 
 # rm -r dist
